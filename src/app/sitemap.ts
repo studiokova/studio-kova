07ec75b8b64a2dde343d6417dee962199207a9e5
Clémence Laurent <hello@studiokova.fr>
2026-05-13 10:03:14 +0200

    SEO first step

---
+++ src/app/sitemap.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://www.studiokova.fr',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: 'https://www.studiokova.fr/quiz',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://www.studiokova.fr/analyse',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.studiokova.fr/surmesure',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+  ]
+}
